3c0f245f1d93118a7230f8fdc32f27d8c32b9f68
kotohata <t.kotohata@gmail.com>
2014-12-11 11:53:17 +0900

    modify

--- app.js
+++ app.js
@@ -3,16 +3,44 @@ $(function() {
     template: '#city_template',
     created: function() {
       console.log(this);
+    },
+    methods: {
+      showDesc: function() {
+        alert('こんにちは');
+      }
+    }
+  });
+  var component = Vue.component('other', {
+    template: '#other_template',
+    created: function() {
+      console.log(this);
+    },
+    methods: {
+      showWarning: function() {
+        alert('エラー');
+      }
+    }
+  });
+  var component = Vue.component('food', {
+    template: '#food_template',
+    created: function() {
+      console.log(this);
+    },
+    methods: {
+      printLog: function(vm) {
+        console.log(vm);
+        console.log(this);
+      }
     }
   });
   var app = new Vue({
     el: '#app',
     data: {
-      title: 'アメリカの都市',
+      title: 'アメリカといえば',
       items: [
-        { text: 'ニューヨーク' },
-        { text: 'シカゴ' },
-        { text: 'サンフランシスコ' }
+        { text: 'ニューヨーク', type: 'city' },
+        { text: '大統領', type: 'other' },
+        { text: 'ハンバーガー', type: 'food' }
       ]
     }
   });
